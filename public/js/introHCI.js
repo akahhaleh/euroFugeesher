403e38401358577d074867f30fb45547bbadb5da
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-02-19 14:46:56 -0800

    init

---
+++ public/js/introHCI.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	// your code here
+	$(".likeBtn").click(projectLiked);
+}
+
+function projectLiked(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    ga("send", "event", 'like', 'click');
+}
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var projects = require('../projects.json');
+
+/*
+ * GET home page.
+ */
+
+exports.view = function(req, res){
+	random_num = Math.random();
+	console.log(random_num);
+	if (random_num >= 0){
+		projects["grid"] = false;
+  		res.render('index', projects);
+	} else {
+		res.redirect('/grid');
+	}
+	
+};
+
+
+exports.viewGrid = function(req, res){
+
+	projects["grid"] = true;
+  	res.render('index', projects);
+};
